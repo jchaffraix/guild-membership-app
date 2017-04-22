685afe3e17ddf72ac9299324448ad16978578cba
Julien Chaffraix <julien.chaffraix@gmail.com>
2017-04-22 16:55:58 -0700

    Add authentication code.
    The code currently doesn't work as we never set the
    authentication headers.

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+import google.oauth2.id_token
 import logging
 import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        template = open('index.html').read()
-        self.response.write(template)
+      try:
+        # TODO: Once I have validated this code, I should probably
+        # move under a single try...except.
+        authorization = self.request.headers['Authorization']
+      except:
+        self.response.write(open('authentication.html').read())
+        return
+
+      id_token = authorization.split(' ').pop()
+      claims = google.oauth2.id_token.verify_firebase_token(
+      id_token, HTTP_REQUEST)
+      if not claims:
+        #self.response.write(open('authentication.html').read())
+        return 'Unauthorized', 401
+
+      # Authenticated user
+      template = open('index.html').read()
+      self.response.write(template)
 
 app = webapp2.WSGIApplication([
   ('/', MainPage),
